2dd9ef5f6e5f04be0fc6920580ef74de328298f8
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2023-02-23 22:17:11 +0700

    headerUI

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import { useRef, useState } from "react";
 
+import { AiFillCloseCircle, AiOutlineSearch } from "react-icons/ai";
+import { CgSpinner } from "react-icons/cg";
+import images from "~/assets/images";
 import * as S from "./styles";
 const Header = () => {
-  return <S.HeaderContainer>Header</S.HeaderContainer>;
+  return (
+    <S.HeaderContainer>
+      <img src={images.logo.default} alt="Tiktok" />
+      <div className="header_search">
+        <input
+          placeholder="search accounts and videos"
+          spellCheck={false}
+          type="text"
+        />
+        <button className="clear_btn">
+          <AiFillCloseCircle className="clear_icon" />
+        </button>
+        <CgSpinner className="loading_icon" />
+        <button className="search_btn">
+          <AiOutlineSearch className="search_icon" />
+        </button>
+      </div>
+      <div className="header_action"></div>
+    </S.HeaderContainer>
+  );
 };
 
 export default Header;
